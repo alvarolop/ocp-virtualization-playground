feff4729a1e99cf29a45b68514ce2584c690a018
Alvaro López Medina <alopezme@redhat.com>
2023-05-03 17:23:59 +0200

    :globe_with_meridians: Skip Terminated machines and check security Groups

--- ocp-tools/add-net-interface.sh
+++ ocp-tools/add-net-interface.sh
@@ -17,14 +17,22 @@ SUBNET_ID=$(aws ec2 describe-subnets \
     --query "Subnets[*].SubnetId" \
     --output text)
 
+SECURITY_GROUP=$(aws ec2 describe-security-groups \
+--region $AWS_DEFAULT_REGION \
+--filters Name=tag:Name,Values=*worker* \
+--query "SecurityGroups[*].GroupId" \
+--output text)
+
 echo "This is the SUBNET_ID: $SUBNET_ID"
+echo "This is the SECURITY_GROUP: $SECURITY_GROUP"
+
 
 # The following command creates an interface in the same private region as the Metal instance is.
 NETWORK_INTERFACE_ID=$(aws ec2 create-network-interface \
     --region $AWS_DEFAULT_REGION \
     --subnet-id $SUBNET_ID \
     --description "Second interface for the VM" \
-    --groups sg-0bf11b08099b2cae3 | jq -r .NetworkInterface.NetworkInterfaceId)
+    --groups $SECURITY_GROUP | jq -r .NetworkInterface.NetworkInterfaceId)
     # --private-ip-address 10.0.2.17
 
 echo "This is the NETWORK_INTERFACE_ID: $NETWORK_INTERFACE_ID"
@@ -32,13 +40,13 @@ echo "This is the NETWORK_INTERFACE_ID: $NETWORK_INTERFACE_ID"
 # This command provides both the instance id and the subnet id. Although we are getting the network in a different way above.
 # INSTANCE_ID=$(aws ec2 describe-instances \
 #     --region $AWS_DEFAULT_REGION \
-#     --filters Name=instance-type,Values=c5.metal \
+#     --filters Name=instance-type,Values=c5.metal  Name=instance-state-code,Values=16 \
 #     --query "Reservations[*].Instances[*].{Instance:InstanceId,Subnet:SubnetId}")
 
 # The following command retrieves the ID of the metal instance
 INSTANCE_ID=$(aws ec2 describe-instances \
     --region $AWS_DEFAULT_REGION \
-    --filters Name=instance-type,Values=c5.metal \
+    --filters Name=instance-type,Values=c5.metal Name=instance-state-code,Values=16 \
     --query "Reservations[*].Instances[*].{Instance:InstanceId}" \
     --output text)
 
